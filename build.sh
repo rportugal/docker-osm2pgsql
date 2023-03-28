5888a0f283949bbc3581493926e2c1037df07ba7
Ricardo Portugal <ricardofmportugal@gmail.com>
2023-03-28 20:30:04 +0100

    push

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 
 docker buildx use "osm2pgsql" || docker buildx create --use --name "osm2pgsql"
 
-docker buildx build --load \
+docker buildx build --push \
    --platform linux/amd64 \
    -t rportugal/osm2pgsql:1.8.1 .
