8d7bcaffa5215e5bd46bbb8cfb29a671746d81fa
Ricardo Portugal <ricardofmportugal@gmail.com>
2023-03-28 20:59:01 +0100

    arm64

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -e
 docker buildx use "osm2pgsql" || docker buildx create --use --name "osm2pgsql"
 
 docker buildx build --push \
-   --platform linux/amd64 \
+   --platform linux/amd64,linux/arm64 \
    -t rportugal/osm2pgsql:1.8.1 .
